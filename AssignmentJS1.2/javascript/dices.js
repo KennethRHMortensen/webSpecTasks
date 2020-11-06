e432fc50cd48feb7822d66fb771e7f2661e40470
Kenneth <kenneth.m@kennt.dk>
2020-11-06 15:34:55 +0100

    added index, styles and js files

---
+++ AssignmentJS1.2/javascript/dices.js
@@ -0,0 +1,27 @@
+//Random number as innerHTML to display a natural number on a die
+//Target and declare variables
+let button = document.getElementById("roll");
+//
+let die = document.getElementsByClassName("dice");
+
+//Create array of numbers for global usage
+let numbers = [];
+
+//Loop 5 dices and display values, while pushing values into an array
+let rollDice = function(){
+    numbers = [];
+    for (i = 0; i < die.length; i++){
+        let number = generateNumber();
+        die[i].innerHTML = number;
+        numbers.push(number);
+    }
+    console.log(numbers);
+}
+
+//Generate five random numbers between 1 and 6 on each die
+let generateNumber = function(){
+    let number = Math.floor((Math.random() * 6) + 1);
+    return number;
+}
+
+button.addEventListener('click', rollDice);
