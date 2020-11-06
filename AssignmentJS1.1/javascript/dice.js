c881a12f3fef71fd2526be168bda23f1069ab20f
Kenneth <kenneth.m@kennt.dk>
2020-11-06 17:01:27 +0100

    edited js file

--- AssignmentJS1.1/javascript/dice.js
+++ AssignmentJS1.1/javascript/dice.js
@@ -1,27 +1,15 @@
 //Random number as innerHTML to display a natural number on a die
 //Target and define aliases for elements from html 
 let button = document.getElementById("roll");
-let number = document.getElementById("number");
+let die = document.getElementById("dice");
 
-//On refresh display ? as number
-number.innerHTML = "?";
-
-//Define integers to Math.random
-let min = 1;
-let max = 7;
-
-//Generate random number between min-max equal to a die
-function rollDice(){
-    let random = Math.floor((Math.random() * (max-min) + min));
+let rollDice = function(){
+    die.innerHTML = generateNumber();
+}
+//generate random integer between 1 and 6 
+let generateNumber = function(){
+    let random = Math.floor((Math.random() * 6) +1);
     return random;
 }
+button.addEventListener('click', rollDice);
 
-//Do function on click of button and display number on die
-button.addEventListener("click", function(){
-    let result = rollDice();
-    number.innerHTML = result;
-})
-
-
-
-//Guided by https://www.youtube.com/watch?v=_jGnrQyvdTQ&t=45s
