5bf5ec2428cb8e5d757ac92e9fd85bc233c78bd1
Kenneth <kenneth.m@kennt.dk>
2020-11-18 14:56:24 +0100

    created tutorial

--- ProjectToDo/javascript/javascript.js
+++ ProjectToDo/javascript/javascript.js
@@ -1,31 +1,23 @@
-/* Todo list */
+/*******  TO DO LIST *******/
+
+// Shortkey for declaring variables through ID's
 const $ = function(id){
     return document.getElementById(id);
 }
-
+// Declarations
 let todoList = $('todoItemList');
 let addBtn = $('addItem');
 let textField = $('inputText');
 let todo = $('todo');
 let saveBtn = $('saveBtn');
-let listBox = $('listBox')
 
-/*let listBox = [];*/
-
-// check if input value is > 0, if yes = run addItem function
-function addItemAfterClick(){
-    if (textField.value.length > 0) {
-        addItem();
-    } else {
-        alert('You forgot to put in some text');
-    }
-}
+// Main function to add item to the list
 function addItem () {
-    
     //  Defines a value based on the users input
     let textInput = $('inputText').value;
 
-    //  Creates a todo list item and divs and adds classnames
+    // CREATE LI ELEMENTS IN THE UL 
+    //  Creates a list item and divs and adds classnames
     const newLi = document.createElement('li');
     newLi.setAttribute('class', 'todoItem');
 
@@ -35,37 +27,33 @@ function addItem () {
     const liBtns = document.createElement('div');
     liBtns.setAttribute('class', 'liBtns');
 
-    //  Inputs user text into the new element
+    // Inputs user text into the new element
     const liUserText = document.createTextNode(textInput);
 
-    //  Displays the new todo list items text
+    // Displays the new todo list items text
     liText.appendChild(liUserText);
 
-    //  Displays new div elements
+    // Displays new div elements
     newLi.append(liText, liBtns);
 
-    //  Displays the new todo list item
+    // Displays the new todo list item
     todoList.append(newLi);
 
-    //  Creates a delete and an edit button with every new item added
+    // CREATE BUTTONS ON LISTED ITEMS
+    // Creates a delete and an edit button with every new item added
     const newDeleteBtn = document.createElement('button');
-    const newEditBtn = document.createElement('button');
-    
-    //  Adds classname to buttons
     newDeleteBtn.setAttribute('class', 'deleteBtn');
-    const deleteBtn = document.createTextNode('X');
     
+    const newEditBtn = document.createElement('button');
     newEditBtn.setAttribute('class', 'editBtn');
-    const editBtn = document.createTextNode('O');
-
-
-    // Displays delete and edit buttons
+        
+    // Appends and displays delete and edit buttons
     liBtns.append(newEditBtn, newDeleteBtn);
-    newDeleteBtn.appendChild(deleteBtn);
-    newEditBtn.appendChild(editBtn);
+    newDeleteBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
+    newEditBtn.innerHTML = '<i class="fas fa-pencil-alt"></i>';
     
-
-    //  Loops through each list item and adds an event listener with a function
+    // HIGHLIGHTING A FINNISHED TASK ON THE UL
+    // Loops through each list item and adds an event listener with a function
     let todoListItms = $('todoItemList').childNodes;
     todoListItms.forEach(function() {
         newDeleteBtn.addEventListener('click', deleteItem);
@@ -73,12 +61,12 @@ function addItem () {
         newEditBtn.addEventListener('click', editItem);
     });
 
-    //  Deletes list item
+    // Deletes list item
     function deleteItem () {
         newLi.remove();
     }
 
-    //  Highlights list items
+    // Highlights list items
     function crossItem () {
         if (newLi.classList.contains('done')){
             newLi.classList.remove('done');
@@ -87,7 +75,7 @@ function addItem () {
         }
     }
 
-    //  Prompts a message to edit an item in case of a mispell
+    // Prompts a message to edit an item in case of a mispell
     function editItem () {
         let txt;
         let newText = prompt('Edit item', liText.innerHTML);
@@ -99,11 +87,36 @@ function addItem () {
         liText.innerHTML = txt;
     }
 
+    //
+    textField.focus();
     // Resets the input field
     textField.value = '';
     } 
-addBtn.addEventListener('click', addItemAfterClick);
+   
+// check if input value is > 0, if yes = run addItem function
+function addItemAfterClick(){
+    if (textField.value.length > 0) {
+        addItem();
+    } else {
+        alert('You forgot to put in some text');
+    }
+}
 
+function addItemAfterKeypress(e){
+    if (textField.value.length > 0 && e.key === 'Enter') {
+      addItem();
+    } else if (textField.value.length == 0 && e.key === 'Enter'){
+        alert('You forgot to put in some text');
+    }
+}
+
+addBtn.addEventListener('click', addItemAfterClick);
+textField.addEventListener('keypress', addItemAfterKeypress);
+
+
+/********* SAVE LISTS **********/
+// Display save button when there's at least one item on the list
+/* 
 function displaySaveBtn(){
     if (todoList.getElementsByTagName('li').length >= 1){
         saveBtn.style.display = "block";
@@ -113,6 +126,7 @@ function displaySaveBtn(){
 }
 window.setInterval(displaySaveBtn, 100);
 
+// save list and add a title to the list
 function saveList(){
     const addDiv = document.createElement('div');
     addDiv.setAttribute('class', 'savedList');
@@ -123,13 +137,21 @@ function saveList(){
 }
 
 function addListToLists(){
-    /*let titleText = prompt('Write the title of your list');
-    if (titleText == null || titleText == ''){*/
+    /*
+    let titleText = prompt('Write the title of your list');
+    if (titleText == null || titleText == ''){ 
         saveList();
 
-        /*const addTitle = document.createElement('h2')
-        addTitle.setAttribute('class', 'listTitle');*/
+        /*
+        const addTitle = document.createElement('h2')
+        addTitle.setAttribute('class', 'listTitle');
 
     }
     saveBtn.addEventListener('click', addListToLists);
 
+*/
+
+/********* LIST BOX ********/
+// let listBox = $('listBox');
+
+// let listBox = [];
