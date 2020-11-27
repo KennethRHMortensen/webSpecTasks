5fa740c62137a432621cb41aaff640780d313cdb
Kenneth <kenneth.m@kennt.dk>
2020-11-27 13:52:48 +0100

    added fully functional addbook function

--- ProjectBookSort/javascript/addBook.js
+++ ProjectBookSort/javascript/addBook.js
@@ -6,6 +6,7 @@ const $ = function(id){
 //Defining global variables
 let addBtn = $('add');
 let addImage = $('addImage');
+let sort = $('sort');
 
 //Add a book to the container
 const addBook = function () {
@@ -23,14 +24,15 @@ const addBook = function () {
         let book = {
             title: $('userTitle').value,
             author: $('userAuthor').value,
-            genre: $('checkbox').value,
             image: $('selectImage').value
         };
+        let genres = checkIfGenre();
+        book.genre = genres;
         books.push(book); //push the objects values into the books array
         let s = JSON.stringify(books); //defines variable as a converted string through JSON of the books object names and values
         localStorage.setItem('books', s); //sets our object 'books' with the values 's' as item to localstorage 
         console.log(`check and display object values that are pushed into books array ${localStorage}`);
-        addBookToContainer(); //add new book with values to container
+        addBookToContainer(genres); //add new book with values to container
         addImageToBook(); //function to change the divs background from default to selected image 
         console.log(`book.title = ${book.title}\nbook.author = ${book.author}\nbook.genre = ${book.genre}\nbook.image = ${book.image}`);
     } else {
@@ -41,17 +43,17 @@ const addBook = function () {
 //create bookshelf to display all added books
 let bookShelf = $('container');
 
-const addBookToContainer = function () {
+const addBookToContainer = function (genres) {
     let book = {
         title: $('userTitle').value,
         author: $('userAuthor').value,
-        genre: $('checkbox').value,
+        genre: genres,
         image: $('selectImage').value
     };
     //create and append books div and info when adding a book
     let newBook = document.createElement('div');
-    newBook.setAttribute('class', 'book');
-    
+    newBook.setAttribute('class', 'book ' + `${book.genre}`);
+
     let newBookPic = document.createElement('div');
     newBookPic.setAttribute('class', 'bookPicture' + 'id', 'bookPic');
     
@@ -110,7 +112,7 @@ if (isLocalStorageEnabled) {
     let book = {
         title: $('userTitle').value,
         author: $('userAuthor').value,
-        genre: $('checkbox').value,
+        genre: document.getElementsByClassName('checked').value,
         image: $('selectImage').value
     };
     let test = JSON.stringify(book);
@@ -122,3 +124,58 @@ if (isLocalStorageEnabled) {
 addBtn.addEventListener('click', addBook);
 addImage.addEventListener('change', addImageToBook, true);
 
+
+const checkIfGenre = function(){
+    let genres='';
+    if(document.querySelector('#thriller:checked')){
+        genres += (document.querySelector('#thriller').value) + ', ';
+    }
+    if(document.querySelector('#crime:checked')){
+        genres += (document.querySelector('#crime').value) + ', '; 
+    }
+    if(document.querySelector('#drama:checked')){
+        genres += (document.querySelector('#drama').value) + ', ';
+    }
+    if(document.querySelector('#adventure:checked')){
+        genres += (document.querySelector('#adventure').value) + ', ';
+    }
+    if(document.querySelector('#scifi:checked')){
+        genres += (document.querySelector('#scifi').value) + ', ';
+    }
+    else if (genres == ''){
+        genres += 'No genre selected';
+    }
+    return genres;
+}
+
+/*const sortByTitle = function(selected){
+    let tempArr = new Array();
+    for (let i = 0; i < selected.options.length; i++){
+        tempArr[i] = new Array();
+        tempArr[i][0] = selected.options[i].value;
+    }
+    tempArr.sort();
+
+}
+const sortBy = function (){
+    let title = $('bookTitle');
+    let author = $('bookAuthor');
+        if(title){
+            let tempArr = new Array();
+            tempArr.push(books)
+
+        }
+        if(author){
+
+        }
+        else{
+
+        }
+    }
+
+function addActivityItem(){
+    //option is selected
+    alert("yeah");
+}
+sort.addEventListener("change", sortBy, false);
+*/
